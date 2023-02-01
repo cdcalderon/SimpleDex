2f4e271bb5f78528d2496ce16e3d7cf4b10496a6
Carlos Calderon <cdcalderon@gmail.com>
2023-01-31 20:13:18 -0600

    add sell method

--- contracts/DEX.sol
+++ contracts/DEX.sol
@@ -2,4 +2,36 @@ pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-contract DEX {}
+contract DEX {
+    IERC20 public associatedToken;
+    uint256 price;
+    address owner;
+
+    constructor(IERC20 _token, uint256 _price) {
+        associatedToken = _token;
+        owner = msg.sender;
+        price = _price;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "you are not the owner");
+        _;
+    }
+
+    function sell() external onlyOwner {
+        uint256 allowance = associatedToken.allowance(
+            msg.sender,
+            address(this)
+        );
+        require(
+            allowance > 0,
+            "you must allow this contract access to at least one token"
+        );
+        bool sent = associatedToken.transferFrom(
+            msg.sender,
+            address(this),
+            allowance
+        );
+        require(sent, "failed to send");
+    }
+}
